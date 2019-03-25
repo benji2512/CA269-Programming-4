03ee3334eb18c7fe327b31a9a1ae797fdc71a0f8
Ben <benthedev@outlook.com>
2019-03-25 18:58:03 +0000

    Just an update commit. Not mush work done lately.

---
+++ Week6/README.md
@@ -0,0 +1,1 @@
+I'll revisit this week in the future as a revision exercise.
---
+++ Week7/inheritance1.java
@@ -0,0 +1,8 @@
+public class inheritance1
+{
+	public static void main(String []args)
+	{
+		Parent x = new Child();
+        System.out.print(x);
+	}
+}
