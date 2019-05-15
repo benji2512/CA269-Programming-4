74b5fd0c31ebd550de1f75bbea333c31f252a76b
ben <benthedev@outlook.com>
2019-05-15 13:48:20 +0000

    Renamed all directories to topics

--- ArraysAndWordGame/WordContainsLetter.java
+++ ArraysAndWordGame/WordContainsLetter.java
@@ -1,5 +1,5 @@
 // Create a Word class with one static method called containsLetter
-public class WordContains{
+public class WordContainsLetter{
     static boolean containsLetter(String word, char letter){
         if(word.length() != 0){
             for(int i=0;i<word.length();i++){
